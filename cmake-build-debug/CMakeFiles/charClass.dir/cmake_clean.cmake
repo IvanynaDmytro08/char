dc741191968bbf6f058fc6abb18093a808023b8f
Ivanyna Dmytro <dimaivanina@MacBook-Pro-Dima.local>
2022-12-05 14:18:41 +0200

    first commit

---
+++ cmake-build-debug/CMakeFiles/charClass.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/charClass.dir/main.cpp.o"
+  "charClass"
+  "charClass.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/charClass.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
